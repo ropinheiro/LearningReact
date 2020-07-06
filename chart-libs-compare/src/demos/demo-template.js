979be727728e3c9b10f554cad19b0c24b64847b7
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-07-06 10:05:47 +0100

    Chart Libs Compare | Add comment separators for each chart, as they tend to have a lot of code in between

--- chart-libs-compare/src/demos/demo-template.js
+++ chart-libs-compare/src/demos/demo-template.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 import ChartsGrid from './charts-grid'
 
+// ============================================================================
+// COPY THIS FILE TO ADD A NEW LIBRARY
+// ============================================================================
+
 export default function DemoTemplate () {
   const demosList = [
     { key: 0, label: 'Line Chart', contents: <DemoLineChart /> },
@@ -14,25 +18,49 @@ export default function DemoTemplate () {
   return <ChartsGrid title='Some Charts Library Demo' demosList={demosList} />
 }
 
+// ============================================================================
+// REMOVE THIS AT THE END
+// ============================================================================
+
 function DemoDefault () {
   return <p>TODO: chart here</p>
 }
+
+// ============================================================================
+// Pie Chart
+// ============================================================================
 
 function DemoLineChart () {
   return <DemoDefault />
 }
 
+// ============================================================================
+// Stacked Area Chart
+// ============================================================================
+
 function DemoStackedAreaChart () {
   return <DemoDefault />
 }
+
+// ============================================================================
+// Pie Chart
+// ============================================================================
 
 function DemoPieChart () {
   return <DemoDefault />
 }
 
+// ============================================================================
+// Bullet Chart
+// ============================================================================
+
 function DemoBulletChart () {
   return <DemoDefault />
 }
+
+// ============================================================================
+// Gantt Chart
+// ============================================================================
 
 function DemoGanttChart () {
   return <DemoDefault />
