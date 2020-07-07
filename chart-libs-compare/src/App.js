79efe06f439f0edf9878e5947d839ee5a65175cf
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-07-07 10:42:46 +0100

    Chart Libs Compare | Install React Google Charts package

--- chart-libs-compare/src/App.js
+++ chart-libs-compare/src/App.js
@@ -2,16 +2,17 @@ import React from 'react'
 import SideMenu from './SideMenu'
 import './App.css'
 
-import DemoTemplate from './demos/demo-template'
 import DemoRecharts from './demos/demo-recharts'
+import DemoReactGoogleCharts from './demos/demo-react-google-charts'
+import DemoTemplate from './demos/demo-template'
 
 // ============================================================================
 // Configure here the demos to be shown.
 // To add a new library, copy the DemoTemplate to a new file.
 // ============================================================================
 const demosList = [
-  { key: 0, label: 'Recharts', contents: <DemoRecharts /> },
-  { key: 1, label: 'Demo #2', contents: <DemoTemplate /> },
+  { key: 0, label: 'Google Charts', contents: <DemoReactGoogleCharts /> },
+  { key: 1, label: 'Recharts', contents: <DemoRecharts /> },
   { key: 2, label: 'Demo #3', contents: <DemoTemplate /> }
 ]
 
