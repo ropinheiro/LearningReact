7d050ca32cacf0ff2ceaba7afc523de75c08248e
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-07-08 09:16:34 +0100

    Chart Libs Compare | Nivo Rocks: add Pie Chart example

--- chart-libs-compare/src/demos/demo-nivo-rocks.js
+++ chart-libs-compare/src/demos/demo-nivo-rocks.js
@@ -4,6 +4,7 @@ import ChartsGrid from './charts-grid'
 
 import { ResponsiveBullet } from '@nivo/bullet'
 import { ResponsiveLine } from '@nivo/line'
+import { ResponsivePie } from '@nivo/pie'
 
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -415,10 +416,155 @@ function DemoStackedAreaChart () {
 // Pie Chart
 // ============================================================================
 
+const dataDemoPieChart = [
+  {
+    id: 'javascript',
+    label: 'javascript',
+    value: 505,
+    color: 'hsl(41, 70%, 50%)'
+  },
+  {
+    id: 'sass',
+    label: 'sass',
+    value: 532,
+    color: 'hsl(231, 70%, 50%)'
+  },
+  {
+    id: 'scala',
+    label: 'scala',
+    value: 154,
+    color: 'hsl(134, 70%, 50%)'
+  },
+  {
+    id: 'php',
+    label: 'php',
+    value: 127,
+    color: 'hsl(129, 70%, 50%)'
+  },
+  {
+    id: 'c',
+    label: 'c',
+    value: 270,
+    color: 'hsl(34, 70%, 50%)'
+  }
+]
+
 function DemoPieChart () {
   return (
     <NivoWrapper>
-      <DemoDefault />
+      <ResponsivePie
+        data={dataDemoPieChart}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        colors={{ scheme: 'nivo' }}
+        borderWidth={1}
+        borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+        radialLabelsSkipAngle={10}
+        radialLabelsTextXOffset={6}
+        radialLabelsTextColor='#333333'
+        radialLabelsLinkOffset={0}
+        radialLabelsLinkDiagonalLength={16}
+        radialLabelsLinkHorizontalLength={24}
+        radialLabelsLinkStrokeWidth={1}
+        radialLabelsLinkColor={{ from: 'color' }}
+        slicesLabelsSkipAngle={10}
+        slicesLabelsTextColor='#333333'
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+        defs={[
+          {
+            id: 'dots',
+            type: 'patternDots',
+            background: 'inherit',
+            color: 'rgba(255, 255, 255, 0.3)',
+            size: 4,
+            padding: 1,
+            stagger: true
+          },
+          {
+            id: 'lines',
+            type: 'patternLines',
+            background: 'inherit',
+            color: 'rgba(255, 255, 255, 0.3)',
+            rotation: -45,
+            lineWidth: 6,
+            spacing: 10
+          }
+        ]}
+        fill={[
+          {
+            match: {
+              id: 'ruby'
+            },
+            id: 'dots'
+          },
+          {
+            match: {
+              id: 'c'
+            },
+            id: 'dots'
+          },
+          {
+            match: {
+              id: 'go'
+            },
+            id: 'dots'
+          },
+          {
+            match: {
+              id: 'python'
+            },
+            id: 'dots'
+          },
+          {
+            match: {
+              id: 'scala'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+              id: 'lisp'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+              id: 'elixir'
+            },
+            id: 'lines'
+          },
+          {
+            match: {
+              id: 'javascript'
+            },
+            id: 'lines'
+          }
+        ]}
+        legends={[
+          {
+            anchor: 'bottom',
+            direction: 'row',
+            translateY: 56,
+            itemWidth: 100,
+            itemHeight: 18,
+            itemTextColor: '#999',
+            symbolSize: 18,
+            symbolShape: 'circle',
+            effects: [
+              {
+                on: 'hover',
+                style: {
+                  itemTextColor: '#000'
+                }
+              }
+            ]
+          }
+        ]}
+      />
     </NivoWrapper>
   )
 }
