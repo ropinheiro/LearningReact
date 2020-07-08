50c3c4b321f92b164a6f7d50f0de58c8f402af20
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2020-07-08 11:55:54 +0100

    Chart Libs Compare | React Vis: add demos page and Line Chart example

--- chart-libs-compare/src/App.js
+++ chart-libs-compare/src/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import SideMenu from './SideMenu'
 import './App.css'
 
-import DemoReactGoogleCharts from './demos/demo-react-google-charts'
-import DemoRecharts from './demos/demo-recharts'
 import DemoNivoRocks from './demos/demo-nivo-rocks'
+import DemoReactGoogleCharts from './demos/demo-react-google-charts'
+import DemoReactVis from './demos/demo-react-vis'
+import DemoRecharts from './demos/demo-recharts'
 import DemoTemplate from './demos/demo-template'
 
 // ============================================================================
@@ -12,10 +13,11 @@ import DemoTemplate from './demos/demo-template'
 // To add a new library, copy the DemoTemplate to a new file.
 // ============================================================================
 const demosList = [
-  { key: 0, label: 'Nivo Rocks', contents: <DemoNivoRocks /> },
-  { key: 1, label: 'Google Charts', contents: <DemoReactGoogleCharts /> },
-  { key: 2, label: 'Recharts', contents: <DemoRecharts /> },
-  { key: 3, label: 'Template', contents: <DemoTemplate /> }
+  { key: 0, label: 'React Vis', contents: <DemoReactVis /> },
+  { key: 1, label: 'Nivo Rocks', contents: <DemoNivoRocks /> },
+  { key: 2, label: 'Google Charts', contents: <DemoReactGoogleCharts /> },
+  { key: 3, label: 'Recharts', contents: <DemoRecharts /> },
+  { key: 4, label: 'Template', contents: <DemoTemplate /> }
 ]
 
 function App () {
